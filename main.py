37918f66170091863c75dc39368aecff91dcdaf0
junaid1097 <mdj804187@gmail.com>
2025-07-03 08:52:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,17 @@ CHECK_INTERVAL = 60
 MIN_PAYOUT = 75
 
 COOKIES = {
+    # তোমার cookies এখানে থাকবে
     '__cf_bm': '.y1Ox7KfhgqlLHY.zIHHEaPI0N.vr691J7XkyUKZ81g-1750399653-1.0.1.1-HhWfuhxiDWChXjGZRaRTJEMPiVs3hlYx.RI82odZjNMJXoTmmOLnqZti59fp7EtSuHziUSzCmLPqsL0J3tdrmR1hzFvjEHuFEqz2.0kAssg',
     '__vid1': 'af220b0fe006f49b69d0a9deafcf52f8',
     '_ga_L4T5GBPFHJ': 'GS2.1.s1750398649$o8$g1$t1750399646$j60$l0$h0',
     '_ga': 'GA1.1.835734851.1750158089',
     'activeAccount': 'demo',
     'lang': 'en',
-    'laravel_session': 'eyJpdiI6Im5VY0o1Snpybm5pMHBjZ1hnQ3Ivanc9PSIsInZhbHVlIjoiN1JHRGJ6ZnFEb000Z3FFVldmTkxqd0NIL0VBSkJ0WHI2MEZEc1VZMVdXT3lFYjVnajVFdlNSMU52Ny9vQklScGxvT1VGbG5HTW5hVnZKeEpCSzk2M3o4UStXS1ZCNU5BbktLUU8rQTZaR3cxenhZb2RBL0YvcXRJc2VHR1h1LzUiLCJtYWMiOiJhOTI3M2JmMmUwNDZjNjBjNWVmYWU4OTlkMmM2NjJlZGIxZTU3MTFmMzJiMmJhMDY1M2Y4NjI2MDZiNTg3NzkwIiwidGFnIjoiIn0%3D',
-    'nas': '[%22USDCHF%22%2C%22AUDNZD_otc%22%2C%22USDPKR_otc%22%2C%22USDMXN_otc%22%2C%22USDDZD_otc%22%2C%22GBPUSD%22%2C%22USDJPY%22%2C%22EURJPY%22]',
-    'OTCTooltip': '{%22value%22:false}',
-    'remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d': 'eyJpdiI6IjArc2hCaUhoT2labjV0djlEQnQ0anc9PSIsInZhbHVlIjoiSm9EM2J3MzY4d25NeWlmV08xS0ZsU3daMEpsSVhEdlN6M0pqaW91bjNJYmtUN1h1WjM3RGJhalQ5Y2M4RFkxelAwdU1RQTgzbUxFclVYeVhqblVlcnIrY3hKWWZjRktKUmE4elM3Q0NHRHdwVTZ4WlVTRXYrMGVkbkRDNjZ2RWh2dS81VHRaNmZWV2F6QTlUUWwzd1ZtM0ZkSkxSVHNsOXMwdTRYREtlZGJCN2J2dnUvbGZlSlY2OVp4ZGVta29IcitSenRoZFFkTHJyN3k4NVVOL1UxbisrY01qVDN0blIvQUNBNzdwRjZLbmVkWndiVGY2LzRaQW5wWDNLQTRucyIsIm1hYyI6IjllMjVlODc4ZjA2ZjJhMTA2NTAyNmM3ZWQ0YzAzNTlhYmY0NDI3ZTU3YjgxZmRiMDBkODlhNDY2ZGRiNmViODgiLCJ0YWciOiIifQ==',
+    'laravel_session': '...',
+    'nas': '[...]',
+    'OTCTooltip': '{...}',
+    'remember_web_...': '...',
     'z': '[[%22graph%22%2C2%2C0%2C0%2C0.7701333]]'
 }
 
@@ -34,8 +35,11 @@ def get_driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    
+    # 🟩 IMPORTANT: এখানে double 'chrome114/chrome114' আছে কারণ unzip এ nested folder এসেছে
     options.binary_location = os.path.abspath("chrome114/chrome114/chrome-linux64/chrome")
     service = Service(executable_path=os.path.abspath("chrome114/chrome114/chrome-linux64/chromedriver"))
+    
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
@@ -92,3 +96,4 @@ def run_bot():
 
 if __name__ == '__main__':
     run_bot()
+
